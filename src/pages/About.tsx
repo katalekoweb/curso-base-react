64dd8e754b0fcd7a85b8bb91a8831e580cc433d8
Kataleko <katalekoweb@gmail.com>
2025-10-22 10:34:48 +0100

    add page layout to about page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import './About.css'
+import PageLayout from '../shared/page-layout/PageLayout'
 
 const About = () => {
   return (
-    <div>
+    <PageLayout title='Sobre'>
       Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum veniam alias perspiciatis culpa! Aut aspernatur et necessitatibus nihil culpa. Repellendus architecto amet consequatur odit quo nesciunt fugit esse tempora eos.
-    </div>
+    </PageLayout>
   )
 }
 
